e153f28b600df4f61fecd03f1103684c11318135
bruhify32 <preetileihaorambam2004@gmail.com>
2021-04-27 21:03:14 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/doge.jpeg" width="600">
+  <AboutMe msg="Hiii i m bruhhhloard_32 🐶"/>
+  <DogFacts />
+</template>
+
+<script>
+import AboutMe from './components/AboutMe.vue'
+import DogFacts from './components/DogFacts.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AboutMe,
+    DogFacts
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/DogFacts.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <div>
+      <h1>Dog Facts</h1>
+    <ul>
+        <li>1. Dogs 🐶 love to Code.....</li>
+        <li>2. They eat a lot.</li>
+        <li>3. They like sleep.</li>
+        <li>4. Hate 🐱 cat!!!</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DogFacts'
+  
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+a {
+  color: #42b983;
+}
+</style>
